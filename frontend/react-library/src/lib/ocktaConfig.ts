b474c8304dd302234658d51b2b0b567f2b59e20d
umair beig <umairbeig@gmail.com>
2023-05-07 13:51:42 +0530

    okta

--- frontend/react-library/src/lib/ocktaConfig.ts
+++ frontend/react-library/src/lib/ocktaConfig.ts
@@ -1,8 +1,10 @@
 export const oktaConfig = {
-    clientId: '0oa91wp8dopyNe9Fv5d7',
-    issuer: 'https://dev-10835870.okta.com/oauth2/default',
+    clientId: '0oa9ff0rnahZhKh2c5d7',
+    issuer: 'https://dev-71016337.okta.com/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
 }
+// usertest1234
+// testuser@email.com
